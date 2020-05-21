121ba44988b561b74f404874971a91233830ceb5
Rosie <rosannainman@gmail.com>
2020-05-21 17:57:25 +0100

    Add additional tests

--- time-converter.test.js
+++ time-converter.test.js
@@ -2,4 +2,9 @@ const secondsToNearestFiveMin = require("./time-converter");
 
 test('Rounds time in seconds to nearest 5 minute interval', () => {
     expect(secondsToNearestFiveMin(442)).toBe("10 minutes");
+    expect(secondsToNearestFiveMin(0)).toBe("0 minutes");
+    expect(secondsToNearestFiveMin(1)).toBe("5 minutes");
+    expect(secondsToNearestFiveMin(300)).toBe("5 minutes");
+    expect(secondsToNearestFiveMin(301)).toBe("10 minutes");
+    expect(secondsToNearestFiveMin(3000)).toBe("50 minutes");
 });
