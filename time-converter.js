3156036af9c0634c289553574348fe29aa5a89db
Rosie <rosannainman@gmail.com>
2020-05-21 17:54:54 +0100

    Update function to pass test

--- time-converter.js
+++ time-converter.js
@@ -1,5 +1,19 @@
 secondsToNearestFiveMin = (seconds) => {
 
+    // Divide input by 300 to find number of 5 minute intervals
+    let totalFiveMinIntervals = seconds/300;
+
+    // Round up to whole number
+    totalFiveMinIntervals = Math.ceil(totalFiveMinIntervals);
+
+    // Multiply by 5 to get total minutes
+    let totalMins = totalFiveMinIntervals*5;
+
+    // Return total mins + 'minutes' string
+
+    return totalMins + " minutes";
 }
+
+secondsToNearestFiveMin(442);
 
 module.exports = secondsToNearestFiveMin;
