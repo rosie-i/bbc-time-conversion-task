19042569698f66da83b15c68f1518f85e7773611
Rosie <rosannainman@gmail.com>
2020-05-22 12:49:47 +0100

    Add test for invalid input

--- time-converter.test.js
+++ time-converter.test.js
@@ -7,4 +7,10 @@ test("Rounds time in seconds to nearest 5 minute interval", () => {
     expect(secondsToNearestFiveMin(300)).toBe("5 minutes");
     expect(secondsToNearestFiveMin(301)).toBe("10 minutes");
     expect(secondsToNearestFiveMin(3000)).toBe("50 minutes");
+    expect(secondsToNearestFiveMin(6000)).toBe("100 minutes");
+});
+
+test("Invalid inputs will return error message", () => {
+    expect(secondsToNearestFiveMin("hello")).toBe("Invalid input. Input must be a positive number.");
+    expect(secondsToNearestFiveMin(-50)).toBe("Invalid input. Input must be a positive number.");
 });
